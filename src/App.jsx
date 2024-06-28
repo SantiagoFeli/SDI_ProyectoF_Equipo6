51ed291a7351deace40634af679ebcf55c94231b
SantiagoFeli <sfeliciani@correo.unimet.edu.ve>
2024-06-28 16:45:28 -0400

    cambio de cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import app from "./firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import './App.css'
 
+const auth = getAuth(app);
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [usuario, setUsuario] = useState(null);
+
+  onAuthStateChanged(auth, (usuarioFirebase)=>{
+    if (usuarioFirebase){
+      setUsuario(usuarioFirebase);
+    }
+    else{
+      setUsuario(null);
+    }
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home correoUsuario={usuario ? usuario.email : null}/>} />
+          {/* <Route exact path="/menu" element={<Menu />} /> */}
+          <Route exact path="/login" element={usuario ? <Navigate to="/" replace />: <Login />} />
+          <Route exact path="/register" element={usuario ? <Navigate to="/" replace />: <Register />} />
+          <Route exact path="/LoginAdmin" element={usuario ? <Navigate to="/" replace />: <LoginAdmin/>} />
+          <Route exact path="/contactanos" element={<contactanos />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+      extend: {
+  
+        colors: {
+          'custom-yellow': '#F4D06F',
+          'orangehs': '#ff8811', 
+          'creamhs': '#FFF8F0', 
+          'aquahs': '#9DD9D2', 
+          'aqua-darker': '#76a09c', 
+        },
+  
+        fontFamily: {
+          'montserrat': ['Montserrat', 'sans-serif'],
+        }
+      },
+    },
+    plugins: [require('flowbite/plugin')],
+  }
